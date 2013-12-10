b0fbfe77cee91b3a1f7ea2cc6671f6dbe8d3bbd1
zrivest <zrivest@gmail.com>
2013-12-10 15:28:20 -0600

    added form to create bets, added firebase(broken)

--- app/js/app.js
+++ app/js/app.js
@@ -7,10 +7,12 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
+  'firebase'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view1', {templateUrl: 'partials/create-bet.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl3'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,20 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', [])
+  .controller('MyCtrl1', ['$scope', 'angularFire',function() {
+    var ref = new Firebase("https://quarter-bets.firebaseio.com/");
+    $scope.bets = [];
+    angularFire(ref, $scope, "bets");
+    $scope.addBet = function(e) {
+      if (e.keyCode != 13) return;
+      $scope.bets.push({from: $scope.bet, body: $scope.wager});
+      $scope.bet = "";
+    };
+  }])
+  .controller('MyCtrl2', ['angularFire', function() {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MyCtrl3', ['angularFire', function() {
 
   }]);
