75c4f58a42415b007d40d8cad4e856d2ffc98f5a
zrivest <zrivest@gmail.com>
2013-12-11 16:43:56 -0600

    have pending and active bets columns(active doesn't persist)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,7 +7,7 @@ var quartersControllers = angular.module('quartersControllers', []);
 quartersControllers.controller('MyCtrl1', ['$scope', 'angularFire',
   function($scope, angularFire){
     var ref = new Firebase("https://quarter-bets-whammy.firebaseio.com/");
-    
+
     $scope.inactiveBetsList = [];
     $scope.activeBetsList = [];
     angularFire(ref, $scope, "inactiveBetsList", "activeBetsList");
@@ -15,18 +15,19 @@ quartersControllers.controller('MyCtrl1', ['$scope', 'angularFire',
     $scope.addBet = function(betInfo) {
       $scope.inactiveBetsList = $scope.inactiveBetsList.clean();
       $scope.activeBetsList = $scope.activeBetsList.clean();
+
       var params = angular.copy(betInfo)
       $scope.inactiveBetsList.push({description: params.description, wagerAmount: params.wagerAmount});
       $scope.master = $scope.inactiveBetsList
     };
 
     $scope.takeBet = function(betInfo) {
-      $scope.inactiveBetsList = $scope.inactiveBetsList.clean();
-      $scope.activeBetsList = $scope.activeBetsList.clean();
-      var test = betInfo
+
+      console.log($scope.activeBetsList)
+
       var betparams = angular.copy(betInfo)
       $scope.activeBetsList.push({description: betparams.description, wagerAmount: betparams.wagerAmount});
-      $scope.betparams = test 
+      $scope.betparams = betInfo
     }
 
     Array.prototype.clean = function() {
