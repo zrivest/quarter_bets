7679a0db527e6afb1917e9757d5662bc337722d4
daino3 <dainovu3@gmail.com>
2013-12-11 14:44:59 -0600

    filtered nulls and added take bet button

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,14 +8,33 @@ quartersControllers.controller('MyCtrl1', ['$scope', 'angularFire',
   function($scope, angularFire){
     var ref = new Firebase("https://quarter-bets-whammy.firebaseio.com/");
     
-    $scope.betsList = [];
-    angularFire(ref, $scope, "betsList");
+    $scope.inactiveBetsList = [];
+    $scope.activeBetsList = [];
+    angularFire(ref, $scope, "inactiveBetsList", "activeBetsList");
 
     $scope.addBet = function(betInfo) {
+      $scope.inactiveBetsList = $scope.inactiveBetsList.clean();
+      $scope.activeBetsList = $scope.activeBetsList.clean();
       var params = angular.copy(betInfo)
-      $scope.betsList.push({description: params.description, wagerAmount: params.wagerAmount});
-      $scope.master = $scope.betsList
+      $scope.inactiveBetsList.push({description: params.description, wagerAmount: params.wagerAmount});
+      $scope.master = $scope.inactiveBetsList
     };
+
+    $scope.takeBet = function(betInfo) {
+      $scope.inactiveBetsList = $scope.inactiveBetsList.clean();
+      $scope.activeBetsList = $scope.activeBetsList.clean();
+      var test = betInfo
+      var betparams = angular.copy(betInfo)
+      $scope.activeBetsList.push({description: betparams.description, wagerAmount: betparams.wagerAmount});
+      $scope.betparams = test 
+    }
+
+    Array.prototype.clean = function() {
+      var new_array = this.filter(function(val) {
+       return val !== null && val !== undefined;
+      })
+      return new_array;
+    }
 
 }]);
 
